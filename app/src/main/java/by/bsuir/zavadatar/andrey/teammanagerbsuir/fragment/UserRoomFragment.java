68e3ac2572cbea359e56f2d2fbaa4bae0f85dccb
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-11-26 22:38:31 +0300

    refactor

--- app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/fragment/UserRoomFragment.java
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/fragment/UserRoomFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,6 +24,7 @@ import by.bsuir.zavadatar.andrey.teammanagerbsuir.activity.R;
 
 public class UserRoomFragment extends Fragment implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = UserRoomFragment.class.getName();
     private Context mContext;
     private DrawerLayout mDrawerLayout;
 
@@ -58,7 +60,7 @@ public class UserRoomFragment extends Fragment implements NavigationView.OnNavig
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        inflater.inflate(R.menu.main_activity1234567, menu);
+        inflater.inflate(R.menu.user_room, menu);
     }
 
     @Override
@@ -78,22 +80,13 @@ public class UserRoomFragment extends Fragment implements NavigationView.OnNavig
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@Nullable MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (id){
+            default:
+                Log.d(TAG, "Default Action (no action)");
         }
 
         mDrawerLayout.closeDrawer(GravityCompat.START);
