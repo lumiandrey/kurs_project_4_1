6aa60b5f2e0d0a60a201bcf85b33a6be845dd764
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-11-26 16:04:58 +0300

    add layout with container fragment and abstract class

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/controller/fragments/SingleFragmentActivity.java
@@ -0,0 +1,41 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.controller.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.R;
+
+
+/**
+ * Created by Andrey on 10.11.2016.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    private final static String TAG = SingleFragmentActivity.class.getName();
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+        Log.d(TAG, "called abstract SingleFragmentActivity");
+
+    }
+
+}
