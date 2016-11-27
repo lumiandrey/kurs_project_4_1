2f8faa631468a79e80ad702ba798d36e8d1fd0d0
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-11-28 00:32:10 +0300

    create class convert date

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/utils/DateConvert.java
@@ -0,0 +1,23 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Andrey on 28.11.2016.
+ */
+
+public class DateConvert {
+
+    public static String getDate(Date date){
+        return new SimpleDateFormat("dd MM yyyy").format(date);
+    }
+
+    public static Date getDate(String date) throws ParseException {
+
+        SimpleDateFormat format = new SimpleDateFormat();
+        format.applyPattern("dd.mm.yyyy");
+        return format.parse(date);
+    }
+}
