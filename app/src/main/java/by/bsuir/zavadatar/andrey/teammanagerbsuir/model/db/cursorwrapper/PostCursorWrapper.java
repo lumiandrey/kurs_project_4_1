ad78de0b396141701b02d1885b159f7a678e0b6e
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-11-27 22:53:31 +0300

    create PostCursorWrapper for mapper data with db

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/cursorwrapper/PostCursorWrapper.java
@@ -0,0 +1,35 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.cursorwrapper;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.entity.PostEntity;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.KorpPortalDBSchema;
+
+import static by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.KorpPortalDBSchema.*;
+
+/**
+ * Created by Andrey on 27.11.2016.
+ */
+
+public class PostCursorWrapper extends CursorWrapper {
+    /**
+     * Creates a cursor wrapper.
+     *
+     * @param cursor The underlying cursor to wrap.
+     */
+    public PostCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public PostEntity getPost(){
+
+        PostEntity entity = new PostEntity();
+
+        entity.setIdPost(getInt(getColumnIndex(PostTable.Colums.ID_POST)));
+        entity.setNamePost(getString(getColumnIndex(PostTable.Colums.NAME_POST)));
+        entity.setRate(getDouble(getColumnIndex(PostTable.Colums.RATE)));
+
+        return entity;
+    }
+}
