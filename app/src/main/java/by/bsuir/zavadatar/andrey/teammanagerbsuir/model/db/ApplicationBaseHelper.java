c254873da1912e68e8f07d62779891e7eb62b40d
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-11-27 18:30:08 +0300

    refactor classes

--- app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/ApplicationBaseHelper.java
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/ApplicationBaseHelper.java
@@ -17,8 +17,6 @@ public class ApplicationBaseHelper extends SQLiteOpenHelper {
     private static final int VERSION = 1;
     private static final String DATABASE_NAME = "timebase.db";
 
-    private static ApplicationBaseHelper sApplicationBaseHelper = null;
-
     public ApplicationBaseHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION);
     }
@@ -26,79 +24,79 @@ public class ApplicationBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE country (" +
-                CountryTable.ID_COUNTRY +
+        db.execSQL("CREATE TABLE " + CountryTable.NAME + " (" +
+                CountryTable.Colums.ID_COUNTRY +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                CountryTable.NAME_COUNTRY + " string ," +
-                CountryTable.KEY_CONTRY + " string ," +
-                CountryTable.KEY_PHONE +" string " +
+                CountryTable.Colums.NAME_COUNTRY + " string ," +
+                CountryTable.Colums.KEY_CONTRY + " string ," +
+                CountryTable.Colums.KEY_PHONE +" string " +
                 ")");
 
         Log.d(TAG, "create table (country successfully)");
 
-        db.execSQL("CREATE TABLE city (" +
-                CityTable.ID_CITY +
+        db.execSQL("CREATE TABLE " + CityTable.NAME + " (" +
+                CityTable.Colums.ID_CITY +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                CityTable.NAME + " string," +
-                CityTable.CODE_PHONE + " string," +
-                CityTable.ID_COUNTRY + " integer NOT NULL," +
+                CityTable.Colums.NAME + " string," +
+                CityTable.Colums.CODE_PHONE + " string," +
+                CityTable.Colums.ID_COUNTRY + " integer NOT NULL," +
                 "FOREIGN KEY (id_country) REFERENCES country(id_country)" +
                 ")");
 
         Log.d(TAG, "create table (city successfully)");
 
-        db.execSQL("CREATE TABLE department (" +
-                DepartmentTable.ID_DEPARTMENT +
+        db.execSQL("CREATE TABLE " + DepartmentTable.NAME + " (" +
+                DepartmentTable.Colums.ID_DEPARTMENT +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                DepartmentTable.NAME_DEPARTMENT + " string"+
+                DepartmentTable.Colums.NAME_DEPARTMENT + " string"+
                 ")");
 
         Log.d(TAG, "create table (department successfully)");
 
-        db.execSQL("CREATE TABLE post (" +
-                PostTable.ID_POST +
+        db.execSQL("CREATE TABLE " + PostTable.NAME + " (" +
+                PostTable.Colums.ID_POST +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                PostTable.NAME_POST + " string NOT NULL," +
-                PostTable.RATE + " FLOAT"+
+                PostTable.Colums.NAME_POST + " string NOT NULL," +
+                PostTable.Colums.RATE + " FLOAT"+
                 ")");
 
         Log.d(TAG, "create table (post successfully)");
 
-        db.execSQL("CREATE TABLE type_user (" +
-                TypeUserTable.ID_TYPE +
+        db.execSQL("CREATE TABLE " + TypeUserTable.NAME + " (" +
+                TypeUserTable.Colums.ID_TYPE +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                TypeUserTable.NAME + " string NOT NULL," +
-                TypeUserTable.ACCESS_LEVEL + " integer NOT NULL"+
+                TypeUserTable.Colums.NAME + " string NOT NULL," +
+                TypeUserTable.Colums.ACCESS_LEVEL + " integer NOT NULL"+
                 ")");
 
         Log.d(TAG, "create table (type_user successfully)");
 
-        db.execSQL("CREATE TABLE user (" +
-                UserTable.ID_USER +
+        db.execSQL("CREATE TABLE " + UserTable.NAME + " (" +
+                UserTable.Colums.ID_USER +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                UserTable.LOGIN + " string NOT NULL," +
-                UserTable.PASSWORD + " string NOT NULL," +
-                UserTable.ID_TYPE_USER + " integer NOT NULL," +
+                UserTable.Colums.LOGIN + " string NOT NULL," +
+                UserTable.Colums.PASSWORD + " string NOT NULL," +
+                UserTable.Colums.ID_TYPE_USER + " integer NOT NULL," +
                 "FOREIGN KEY (id_type_user) REFERENCES type_user(id_type)"+
                 ")");
 
         Log.d(TAG, "create table (user successfully)");
 
-        db.execSQL("CREATE TABLE person (" +
-                PersonTable.ID_PERSON +
+        db.execSQL("CREATE TABLE " + PersonTable.NAME + " (" +
+                PersonTable.Colums.ID_PERSON +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                PersonTable.SURNAME + " string ," +
+                PersonTable.Colums.SURNAME + " string ," +
                 PersonTable.NAME + " string ," +
-                PersonTable.PATRONYMIC + " string," +
-                PersonTable.DATE_OF_BIRTH + " DATE," +
-                PersonTable.SEX + " string," +
-                PersonTable.E_MAIL + " string," +
-                PersonTable.ID_USER + " integer NOT NULL," +
-                PersonTable.ID_POST + " integer NOT NULL," +
-                PersonTable.ID_DEPARTMENT + " integer NOT NULL," +
-                PersonTable.ID_CITY + " integer NOT NULL," +
-                PersonTable.MOBILE_PHONE + " string," +
-                PersonTable.HOME_PHONE + " string," +
+                PersonTable.Colums.PATRONYMIC + " string," +
+                PersonTable.Colums.DATE_OF_BIRTH + " DATE," +
+                PersonTable.Colums.SEX + " string," +
+                PersonTable.Colums.E_MAIL + " string," +
+                PersonTable.Colums.ID_USER + " integer NOT NULL," +
+                PersonTable.Colums.ID_POST + " integer NOT NULL," +
+                PersonTable.Colums.ID_DEPARTMENT + " integer NOT NULL," +
+                PersonTable.Colums.ID_CITY + " integer NOT NULL," +
+                PersonTable.Colums.MOBILE_PHONE + " string," +
+                PersonTable.Colums.HOME_PHONE + " string," +
                 "FOREIGN KEY (id_user) REFERENCES user(id_user)," +
                 "FOREIGN KEY (id_post) REFERENCES post(id_post)," +
                 "FOREIGN KEY (id_department) REFERENCES department(id_department)," +
@@ -107,55 +105,55 @@ public class ApplicationBaseHelper extends SQLiteOpenHelper {
 
         Log.d(TAG, "create table (person successfully)");
 
-        db.execSQL("CREATE TABLE type_task (" +
-                TypeTaskTable.ID_TYPE_TASK +
+        db.execSQL("CREATE TABLE " + TypeTaskTable.NAME + " (" +
+                TypeTaskTable.Colums.ID_TYPE_TASK +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                TypeTaskTable.NAME_TYPE + " string NOT NULL"+
+                TypeTaskTable.Colums.NAME_TYPE + " string NOT NULL"+
                 ")");
 
         Log.d(TAG, "create table (type_task successfully)");
 
-        db.execSQL("CREATE TABLE task (" +
-                TaskTable.ID_TASK + " integer PRIMARY KEY AUTOINCREMENT," +
+        db.execSQL("CREATE TABLE " + TaskTable.NAME + " (" +
+                TaskTable.Colums.ID_TASK + " integer PRIMARY KEY AUTOINCREMENT," +
                 TaskTable.NAME + " string ," +
-                TaskTable.DESCRIPTION + " string," +
-                TaskTable.DATE_BEGIN + " DATE ," +
-                TaskTable.DATE_END + " DATE," +
-                TaskTable.DONE + " integer," +
-                TaskTable.PROGRESS + " integer," +
-                TaskTable.ID_TYPE_TASK + " integer NOT NULL," +
-                TaskTable.ID_PERSON_ADD + " integer NOT NULL," +
+                TaskTable.Colums.DESCRIPTION + " string," +
+                TaskTable.Colums.DATE_BEGIN + " DATE ," +
+                TaskTable.Colums.DATE_END + " DATE," +
+                TaskTable.Colums.DONE + " integer," +
+                TaskTable.Colums.PROGRESS + " integer," +
+                TaskTable.Colums.ID_TYPE_TASK + " integer NOT NULL," +
+                TaskTable.Colums.ID_PERSON_ADD + " integer NOT NULL," +
                 "FOREIGN KEY (id_type_task) REFERENCES type_task(id_type_task)," +
                 "FOREIGN KEY (id_person_add) REFERENCES person(id_person)"+
                 ")");
 
         Log.d(TAG, "create table (task successfully)");
 
-        db.execSQL("CREATE TABLE has_task_person (" +
-                HasTaskPersonTable.ID_HAS_TASK_PERSON +
+        db.execSQL("CREATE TABLE " + HasTaskPersonTable.NAME + " (" +
+                HasTaskPersonTable.Colums.ID_HAS_TASK_PERSON +
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                HasTaskPersonTable.ID_TASK + " integer NOT NULL," +
-                HasTaskPersonTable.ID_PERSON + " integer NOT NULL," +
+                HasTaskPersonTable.Colums.ID_TASK + " integer NOT NULL," +
+                HasTaskPersonTable.Colums.ID_PERSON + " integer NOT NULL," +
                 "FOREIGN KEY (id_person) REFERENCES person(id_person)," +
                 "FOREIGN KEY (id_task) REFERENCES task(id_task)"+
                 ")");
 
         Log.d(TAG, "create table (has_task_person successfully)");
 
-        db.execSQL("CREATE TABLE type_activity (" +
-                TypeActivityTable.ID_TYPE_ACTIVITY + " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                TypeActivityTable.NAME_ACTIVITY + " string NOT NULL"+
+        db.execSQL("CREATE TABLE " + TypeActivityTable.NAME + " (" +
+                TypeActivityTable.Colums.ID_TYPE_ACTIVITY + " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                TypeActivityTable.Colums.NAME_ACTIVITY + " string NOT NULL"+
                 ")");
 
         Log.d(TAG, "create table (type_activity successfully)");
 
-        db.execSQL("CREATE TABLE log_time_task (" +
-                LogTimeTaskTable.ID_LOG + " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                LogTimeTaskTable.DATE_LOG + " DATE NOT NULL," +
-                LogTimeTaskTable.DESCRIPTION + " string ," +
-                LogTimeTaskTable.LINKS_EXT_STOR + " string," +
-                LogTimeTaskTable.ID_TYPE_ACTIVITY + " integer NOT NULL," +
-                LogTimeTaskTable.ID_HAS_TASK_PERSON + " integer NOT NULL," +
+        db.execSQL("CREATE TABLE " + LogTimeTaskTable.NAME + " (" +
+                LogTimeTaskTable.Colums.ID_LOG + " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                LogTimeTaskTable.Colums.DATE_LOG + " DATE NOT NULL," +
+                LogTimeTaskTable.Colums.DESCRIPTION + " string ," +
+                LogTimeTaskTable.Colums.LINKS_EXT_STOR + " string," +
+                LogTimeTaskTable.Colums.ID_TYPE_ACTIVITY + " integer NOT NULL," +
+                LogTimeTaskTable.Colums.ID_HAS_TASK_PERSON + " integer NOT NULL," +
                 "FOREIGN KEY (id_type_activity) REFERENCES type_activity(id_type_activity)," +
                 "FOREIGN KEY (id_has_task_person) REFERENCES has_task_person(id_has_task_person)"+
                 ")");
--- app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/KorpPortalDBSchema.java
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/KorpPortalDBSchema.java
@@ -7,94 +7,177 @@ package by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db;
 public class KorpPortalDBSchema {
 
     public static final class CountryTable {
-        public static final String ID_COUNTRY = "id_country";
-        public static final String NAME_COUNTRY = "name_country";
-        public static final String KEY_CONTRY = "key_contry";
-        public static final String KEY_PHONE = "key_phone";
+
+        public static final String NAME = "";
+
+        public static final class Colums{
+            public static final String ID_COUNTRY = "id_country";
+            public static final String NAME_COUNTRY = "name_country";
+            public static final String KEY_CONTRY = "key_contry";
+            public static final String KEY_PHONE = "key_phone";
+        }
+
     }
 
     public static final class CityTable {
-        public static final String ID_CITY = "id_city";
-        public static final String NAME = "name";
-        public static final String CODE_PHONE = "code_phone";
-        public static final String ID_COUNTRY = "id_country";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_CITY = "id_city";
+            public static final String NAME = "name";
+            public static final String CODE_PHONE = "code_phone";
+            public static final String ID_COUNTRY = "id_country";
+        }
+
+
     }
 
     public static final class DepartmentTable {
-        public static final String ID_DEPARTMENT = "id_department";
-        public static final String NAME_DEPARTMENT = "name_department";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_DEPARTMENT = "id_department";
+            public static final String NAME_DEPARTMENT = "name_department";
+        }
+
+
     }
 
     public static final class PostTable {
-        public static final String ID_POST = "id_post";
-        public static final String NAME_POST = "name_post";
-        public static final String RATE = "rate";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_POST = "id_post";
+            public static final String NAME_POST = "name_post";
+            public static final String RATE = "rate";
+        }
+
+
     }
 
     public static final class TypeUserTable {
-        public static final String ID_TYPE = "id_type";
-        public static final String NAME = "name";
-        public static final String ACCESS_LEVEL = "access_level";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_TYPE = "id_type";
+            public static final String NAME = "name";
+            public static final String ACCESS_LEVEL = "access_level";
+        }
+
+
     }
 
     public static final class UserTable {
-        public static final String ID_USER = "id_user";
-        public static final String LOGIN = "login";
-        public static final String PASSWORD = "password";
-        public static final String ID_TYPE_USER = "id_type_user";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_USER = "id_user";
+            public static final String LOGIN = "login";
+            public static final String PASSWORD = "password";
+            public static final String ID_TYPE_USER = "id_type_user";
+        }
+
+
     }
 
     public static final class PersonTable {
-        public static final String ID_PERSON = "id_person";
-        public static final String SURNAME = "surname";
-        public static final String NAME = "name";
-        public static final String PATRONYMIC = "patronymic";
-        public static final String DATE_OF_BIRTH = "date_of_birth";
-        public static final String SEX = "sex";
-        public static final String E_MAIL = "e_mail";
-        public static final String ID_USER = "id_user";
-        public static final String ID_POST = "id_post";
-        public static final String ID_DEPARTMENT = "id_department";
-        public static final String ID_CITY = "id_city";
-        public static final String MOBILE_PHONE = "mobile_phone";
-        public static final String HOME_PHONE = "home_phone";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_PERSON = "id_person";
+            public static final String SURNAME = "surname";
+            public static final String NAME = "name";
+            public static final String PATRONYMIC = "patronymic";
+            public static final String DATE_OF_BIRTH = "date_of_birth";
+            public static final String SEX = "sex";
+            public static final String E_MAIL = "e_mail";
+            public static final String ID_USER = "id_user";
+            public static final String ID_POST = "id_post";
+            public static final String ID_DEPARTMENT = "id_department";
+            public static final String ID_CITY = "id_city";
+            public static final String MOBILE_PHONE = "mobile_phone";
+            public static final String HOME_PHONE = "home_phone";
+        }
+
+
     }
 
     public static final class TypeTaskTable {
-        public static final String ID_TYPE_TASK = "id_type_task";
-        public static final String NAME_TYPE = "name_type";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_TYPE_TASK = "id_type_task";
+            public static final String NAME_TYPE = "name_type";
+        }
+
+
     }
 
     public static final class TaskTable {
-        public static final String ID_TASK = "id_task";
-        public static final String NAME = "name";
-        public static final String DESCRIPTION = "description";
-        public static final String DATE_BEGIN = "date_begin";
-        public static final String DATE_END = "date_end";
-        public static final String DONE = "done";
-        public static final String PROGRESS = "progress";
-        public static final String ID_TYPE_TASK = "id_type_task";
-        public static final String ID_PERSON_ADD = "id_person_add";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_TASK = "id_task";
+            public static final String NAME = "name";
+            public static final String DESCRIPTION = "description";
+            public static final String DATE_BEGIN = "date_begin";
+            public static final String DATE_END = "date_end";
+            public static final String DONE = "done";
+            public static final String PROGRESS = "progress";
+            public static final String ID_TYPE_TASK = "id_type_task";
+            public static final String ID_PERSON_ADD = "id_person_add";
+        }
+
+
     }
 
     public static final class HasTaskPersonTable {
-        public static final String ID_HAS_TASK_PERSON = "id_has_task_person";
-        public static final String ID_TASK = "id_task";
-        public static final String ID_PERSON = "id_person";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_HAS_TASK_PERSON = "id_has_task_person";
+            public static final String ID_TASK = "id_task";
+            public static final String ID_PERSON = "id_person";
+        }
+
+
     }
 
     public static final class TypeActivityTable {
-        public static final String ID_TYPE_ACTIVITY = "id_type_activity";
-        public static final String NAME_ACTIVITY = "name_activity";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_TYPE_ACTIVITY = "id_type_activity";
+            public static final String NAME_ACTIVITY = "name_activity";
+        }
+
+
     }
 
     public static final class LogTimeTaskTable {
-        public static final String ID_LOG = "id_log";
-        public static final String DATE_LOG = "date_log";
-        public static final String DESCRIPTION = "";
-        public static final String LINKS_EXT_STOR = "links_ext_stor";
-        public static final String ID_TYPE_ACTIVITY = "id_type_activity";
-        public static final String ID_HAS_TASK_PERSON = "id_has_task_person";
+
+        public static final String NAME = "";
+
+        public static final class Colums {
+            public static final String ID_LOG = "id_log";
+            public static final String DATE_LOG = "date_log";
+            public static final String DESCRIPTION = "";
+            public static final String LINKS_EXT_STOR = "links_ext_stor";
+            public static final String ID_TYPE_ACTIVITY = "id_type_activity";
+            public static final String ID_HAS_TASK_PERSON = "id_has_task_person";
+        }
+
+
     }
 
 }
