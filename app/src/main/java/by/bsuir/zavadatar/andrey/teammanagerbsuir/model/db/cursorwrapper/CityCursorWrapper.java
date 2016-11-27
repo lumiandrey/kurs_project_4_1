d5294c04bfe005a4c0506f6dfa1c82a49992ab02
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-11-27 21:47:42 +0300

    create CityCursorWrapper for mapper data with db

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/cursorwrapper/CityCursorWrapper.java
@@ -0,0 +1,37 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.cursorwrapper;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.entity.CityEntity;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.KorpPortalDBSchema;
+
+import static by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.KorpPortalDBSchema.*;
+
+/**
+ * Created by Andrey on 27.11.2016.
+ */
+
+public class CityCursorWrapper extends CursorWrapper {
+
+    /**
+     * Creates a cursor wrapper.
+     *
+     * @param cursor The underlying cursor to wrap.
+     */
+    public CityCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public CityEntity getCity(){
+
+        CityEntity cityEntity = new CityEntity();
+
+        cityEntity.setIdCity(getInt(getColumnIndex(CityTable.Colums.ID_CITY)));
+        cityEntity.setName(getString(getColumnIndex(CityTable.Colums.NAME)));
+        cityEntity.setCodePhone(getString(getColumnIndex(CityTable.Colums.CODE_PHONE)));
+        cityEntity.setIdCountry(getInt(getColumnIndex(CityTable.Colums.ID_CITY)));
+
+        return cityEntity;
+    }
+}
