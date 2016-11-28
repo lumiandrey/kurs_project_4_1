817714344e5c06ba2511adc0c52bf5cce6061923
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-11-28 09:28:55 +0300

    create class DatePicker and TimePicker

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/fragment/TimePickerFragment.java
@@ -0,0 +1,88 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.fragment;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TimePicker;
+
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Date;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.activity.R;
+
+/**
+ * Created by Andrey on 12.11.2016.
+ */
+
+public class TimePickerFragment extends AppCompatDialogFragment {
+
+    private static final String ARG_TIME = "time";
+    public static final String EXTRA_DATE = "criminalintent.time";
+
+    private TimePicker mTimePicker;
+
+    public static TimePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_TIME, date);
+
+        TimePickerFragment fragment = new TimePickerFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        Date date = (Date) getArguments().getSerializable(ARG_TIME);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_time, null);
+
+        mTimePicker = (TimePicker) v.findViewById(R.id.dialog_time_time_picker);
+        mTimePicker.setCurrentHour(hour);
+        mTimePicker.setCurrentMinute(minute);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.time_picker_title)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                int hour = mTimePicker.getCurrentHour();
+                                int minute = mTimePicker.getCurrentMinute();
+                                Time time = new Time(hour, minute, 0);
+                                sendResult(Activity.RESULT_OK, time);
+                            }
+
+                        })
+                .create();
+
+    }
+
+    private void sendResult(int resultCode, Date date) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, date);
+
+        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
+    }
+
+}
