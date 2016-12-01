7550e44236b36ecfbc209b5978baefb59834bedc
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-12-02 01:43:16 +0300

    change setter foreing key db

--- app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/BaseHelper.java
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/BaseHelper.java
@@ -35,7 +35,7 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE " + CountryTable.NAME + " (" +
                 CountryTable.Colums.ID_COUNTRY +
-                " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 CountryTable.Colums.NAME_COUNTRY + " string ," +
                 CountryTable.Colums.KEY_CONTRY + " string ," +
                 CountryTable.Colums.KEY_PHONE +" string " +
@@ -48,7 +48,7 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
                 " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 CityTable.Colums.NAME + " string," +
                 CityTable.Colums.CODE_PHONE + " string," +
-                CityTable.Colums.ID_COUNTRY + " integer NOT NULL," +
+                CityTable.Colums.ID_COUNTRY + " INTEGER NOT NULL," +
                 CityTable.REF_COUNTRY +
                 ")");
 
@@ -56,7 +56,7 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE " + DepartmentTable.NAME + " (" +
                 DepartmentTable.Colums.ID_DEPARTMENT +
-                " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 DepartmentTable.Colums.NAME_DEPARTMENT + " string"+
                 ")");
 
@@ -64,7 +64,7 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE " + PostTable.NAME + " (" +
                 PostTable.Colums.ID_POST +
-                " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 PostTable.Colums.NAME_POST + " string NOT NULL," +
                 PostTable.Colums.RATE + " FLOAT"+
                 ")");
@@ -73,19 +73,19 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE " + TypeUserTable.NAME + " (" +
                 TypeUserTable.Colums.ID_TYPE +
-                " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 TypeUserTable.Colums.NAME + " string NOT NULL," +
-                TypeUserTable.Colums.ACCESS_LEVEL + " integer NOT NULL"+
+                TypeUserTable.Colums.ACCESS_LEVEL + " INTEGER NOT NULL"+
                 ")");
 
         Log.d(TAG, "create table (type_user successfully)");
 
         db.execSQL("CREATE TABLE " + UserTable.NAME + " (" +
                 UserTable.Colums.ID_USER +
-                " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 UserTable.Colums.LOGIN + " string NOT NULL," +
                 UserTable.Colums.PASSWORD + " string NOT NULL," +
-                UserTable.Colums.ID_TYPE_USER + " integer NOT NULL," +
+                UserTable.Colums.ID_TYPE_USER + " INTEGER NOT NULL," +
                 UserTable.REF_TYPE_USER +
                 ")");
 
@@ -93,17 +93,17 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE " + PersonTable.NAME + " (" +
                 PersonTable.Colums.ID_PERSON +
-                " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 PersonTable.Colums.SURNAME + " string ," +
                 PersonTable.NAME + " string ," +
                 PersonTable.Colums.PATRONYMIC + " string," +
                 PersonTable.Colums.DATE_OF_BIRTH + " DATE," +
                 PersonTable.Colums.SEX + " string," +
                 PersonTable.Colums.E_MAIL + " string," +
-                PersonTable.Colums.ID_USER + " integer NOT NULL," +
-                PersonTable.Colums.ID_POST + " integer NOT NULL," +
-                PersonTable.Colums.ID_DEPARTMENT + " integer NOT NULL," +
-                PersonTable.Colums.ID_CITY + " integer NOT NULL," +
+                PersonTable.Colums.ID_USER + " INTEGER NOT NULL," +
+                PersonTable.Colums.ID_POST + " INTEGER NOT NULL," +
+                PersonTable.Colums.ID_DEPARTMENT + " INTEGER NOT NULL," +
+                PersonTable.Colums.ID_CITY + " INTEGER NOT NULL," +
                 PersonTable.Colums.MOBILE_PHONE + " string," +
                 PersonTable.Colums.HOME_PHONE + " string," +
                 PersonTable.REF_USER + ',' +
@@ -116,22 +116,22 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE " + TypeTaskTable.NAME + " (" +
                 TypeTaskTable.Colums.ID_TYPE_TASK +
-                " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 TypeTaskTable.Colums.NAME_TYPE + " string NOT NULL"+
                 ")");
 
         Log.d(TAG, "create table (type_task successfully)");
 
         db.execSQL("CREATE TABLE " + TaskTable.NAME + " (" +
-                TaskTable.Colums.ID_TASK + " integer PRIMARY KEY AUTOINCREMENT," +
+                TaskTable.Colums.ID_TASK + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 TaskTable.NAME + " string ," +
                 TaskTable.Colums.DESCRIPTION + " string," +
                 TaskTable.Colums.DATE_BEGIN + " DATE ," +
                 TaskTable.Colums.DATE_END + " DATE," +
-                TaskTable.Colums.DONE + " integer," +
-                TaskTable.Colums.PROGRESS + " integer," +
-                TaskTable.Colums.ID_TYPE_TASK + " integer NOT NULL," +
-                TaskTable.Colums.ID_PERSON_ADD + " integer NOT NULL," +
+                TaskTable.Colums.DONE + " INTEGER," +
+                TaskTable.Colums.PROGRESS + " INTEGER," +
+                TaskTable.Colums.ID_TYPE_TASK + " INTEGER NOT NULL," +
+                TaskTable.Colums.ID_PERSON_ADD + " INTEGER NOT NULL," +
                 TaskTable.REF_TYPE_TASK + ',' +
                 TaskTable.REF_PERSON_ADD +
                 ")");
@@ -140,9 +140,9 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("CREATE TABLE " + HasTaskPersonTable.NAME + " (" +
                 HasTaskPersonTable.Colums.ID_HAS_TASK_PERSON +
-                " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
-                HasTaskPersonTable.Colums.ID_TASK + " integer NOT NULL," +
-                HasTaskPersonTable.Colums.ID_PERSON + " integer NOT NULL," +
+                " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                HasTaskPersonTable.Colums.ID_TASK + " INTEGER NOT NULL," +
+                HasTaskPersonTable.Colums.ID_PERSON + " INTEGER NOT NULL," +
                 HasTaskPersonTable.REF_PERSON + ',' +
                 HasTaskPersonTable.REF_TASK +
                 ")");
@@ -150,19 +150,19 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
         Log.d(TAG, "create table (has_task_person successfully)");
 
         db.execSQL("CREATE TABLE " + TypeActivityTable.NAME + " (" +
-                TypeActivityTable.Colums.ID_TYPE_ACTIVITY + " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                TypeActivityTable.Colums.ID_TYPE_ACTIVITY + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 TypeActivityTable.Colums.NAME_ACTIVITY + " string NOT NULL"+
                 ")");
 
         Log.d(TAG, "create table (type_activity successfully)");
 
         db.execSQL("CREATE TABLE " + LogTimeTaskTable.NAME + " (" +
-                LogTimeTaskTable.Colums.ID_LOG + " integer NOT NULL PRIMARY KEY AUTOINCREMENT," +
+                LogTimeTaskTable.Colums.ID_LOG + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
                 LogTimeTaskTable.Colums.DATE_LOG + " DATE NOT NULL," +
                 LogTimeTaskTable.Colums.DESCRIPTION + " string ," +
                 LogTimeTaskTable.Colums.LINKS_EXT_STOR + " string," +
-                LogTimeTaskTable.Colums.ID_TYPE_ACTIVITY + " integer NOT NULL," +
-                LogTimeTaskTable.Colums.ID_HAS_TASK_PERSON + " integer NOT NULL," +
+                LogTimeTaskTable.Colums.ID_TYPE_ACTIVITY + " INTEGER NOT NULL," +
+                LogTimeTaskTable.Colums.ID_HAS_TASK_PERSON + " INTEGER NOT NULL," +
                 LogTimeTaskTable.REF_TYPE_ACTIVITY + ',' +
                 LogTimeTaskTable.REF_HAS_TASK_PERSON +
                 ")");
@@ -176,5 +176,11 @@ abstract public class BaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    @Override
+    public void onConfigure(SQLiteDatabase db) {
+        super.onConfigure(db);
+        db.setForeignKeyConstraintsEnabled(true);
     }
 }
