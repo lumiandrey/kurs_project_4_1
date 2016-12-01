fc57546c6d485a889c23bbda3af8dcf6134b90f0
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-12-02 02:50:33 +0300

    create Post dao implements

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/dao/sqllite/PostDaoLite.java
@@ -0,0 +1,101 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.dao.sqllite;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.util.List;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.KorpPortalDBSchema;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.cursorwrapper.BaseCustomCursorWrapper;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.cursorwrapper.PostCursorWrapper;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.dao.PostDao;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.entity.PostEntity;
+
+import static by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.KorpPortalDBSchema.*;
+
+/**
+ * Created by Andrey on 02.12.2016.
+ */
+
+public class PostDaoLite extends AbstractDaoBase<PostEntity> implements PostDao {
+
+    public static final String TAG = PostDaoLite.class.getName();
+    public static final String NAME_TABLE = PostTable.NAME;
+    public static final String ID_WHERE = PostTable.Colums.ID_POST;
+
+    public PostDaoLite() {
+        super();
+    }
+
+    @Override
+    public long create(PostEntity entity) {
+
+        return super.create(entity, NAME_TABLE);
+    }
+
+    @Override
+    public PostEntity read(int id) {
+
+        return super.read(NAME_TABLE, ID_WHERE + " = ?", new String[]{String.valueOf(id)});
+    }
+
+    @Override
+    public PostEntity update(PostEntity entity) {
+
+        return super.update(entity, NAME_TABLE,
+                ID_WHERE + " = ?",
+                new String[] {String.valueOf(entity.getIdPost())});
+    }
+
+    @Override
+    public int delete(PostEntity entity) {
+
+        return super.delete(NAME_TABLE, ID_WHERE + " = ?",
+                new String[] {String.valueOf(entity.getIdPost())});
+    }
+
+    @Override
+    protected ContentValues getContentValuesNotId(PostEntity userEntity) {
+
+        ContentValues values = new ContentValues();
+
+        values.put(PostTable.Colums.NAME_POST, userEntity.getNamePost());
+        values.put(PostTable.Colums.RATE, userEntity.getRate());
+
+
+        return values;
+    }
+
+    @Override
+    protected ContentValues getContentValuesWithId(PostEntity entity) {
+
+        ContentValues values = getContentValuesNotId(entity);
+
+        values.put(ID_WHERE, entity.getIdPost());
+
+        return values;
+    }
+
+    @Override
+    protected BaseCustomCursorWrapper<PostEntity> getCursorWrapper(Cursor cursor) {
+        return new PostCursorWrapper(cursor);
+    }
+
+    @Override
+    public List<PostEntity> create(List<PostEntity> entities) {
+
+
+        return super.create(entities, NAME_TABLE);
+    }
+
+    @Override
+    public List<PostEntity> reads() {
+
+        return super.reads(NAME_TABLE);
+    }
+
+    @Override
+    public void deleteAll(){
+        super.deleteAll(NAME_TABLE);
+    }
+}
