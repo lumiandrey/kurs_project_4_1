6ebfe574f281e720dd822cf5b5af179654483d38
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-12-02 02:49:51 +0300

    create department dao implements

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/dao/sqllite/DepartmentDaoLite.java
@@ -0,0 +1,99 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.dao.sqllite;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.util.List;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.KorpPortalDBSchema;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.cursorwrapper.BaseCustomCursorWrapper;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.cursorwrapper.DepartmentCursorWrapper;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.dao.DepartmentDao;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.entity.DepartmentEntity;
+
+import static by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.KorpPortalDBSchema.*;
+
+/**
+ * Created by Andrey on 02.12.2016.
+ */
+
+public class DepartmentDaoLite extends AbstractDaoBase<DepartmentEntity> implements DepartmentDao {
+
+    public static final String TAG = DepartmentDaoLite.class.getName();
+    public static final String NAME_TABLE = DepartmentTable.NAME;
+    public static final String ID_WHERE = DepartmentTable.Colums.ID_DEPARTMENT;
+
+    public DepartmentDaoLite() {
+        super();
+    }
+
+    @Override
+    public long create(DepartmentEntity userEntity) {
+
+        return super.create(userEntity, NAME_TABLE);
+    }
+
+    @Override
+    public DepartmentEntity read(int id) {
+
+        return super.read(NAME_TABLE, ID_WHERE + " = ?", new String[]{String.valueOf(id)});
+    }
+
+    @Override
+    public DepartmentEntity update(DepartmentEntity entity) {
+
+        return super.update(entity, NAME_TABLE,
+                ID_WHERE + " = ?",
+                new String[] {String.valueOf(entity.getIdDepartment())});
+    }
+
+    @Override
+    public int delete(DepartmentEntity entity) {
+
+        return super.delete(NAME_TABLE, ID_WHERE + " = ?",
+                new String[] {String.valueOf(entity.getIdDepartment())});
+    }
+
+    @Override
+    protected ContentValues getContentValuesNotId(DepartmentEntity entity) {
+
+        ContentValues values = new ContentValues();
+
+        values.put(DepartmentTable.Colums.NAME_DEPARTMENT, entity.getNameDepartment());
+
+        return values;
+    }
+
+    @Override
+    protected ContentValues getContentValuesWithId(DepartmentEntity entity) {
+
+        ContentValues values = getContentValuesNotId(entity);
+
+        values.put(CountryTable.Colums.ID_COUNTRY, entity.getIdDepartment());
+
+        return values;
+    }
+
+    @Override
+    protected BaseCustomCursorWrapper<DepartmentEntity> getCursorWrapper(Cursor cursor) {
+        return new DepartmentCursorWrapper(cursor);
+    }
+
+    @Override
+    public List<DepartmentEntity> create(List<DepartmentEntity> entities) {
+
+
+        return super.create(entities, NAME_TABLE);
+    }
+
+    @Override
+    public List<DepartmentEntity> reads() {
+
+        return super.reads(NAME_TABLE);
+    }
+
+    @Override
+    public void deleteAll(){
+        super.deleteAll(NAME_TABLE);
+    }
+}
