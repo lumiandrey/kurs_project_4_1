0ef70262690becb9e4f998aee6484527104b83ac
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-12-02 12:02:00 +0300

    refactor date convert

--- app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/utils/DateConvert.java
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/utils/DateConvert.java
@@ -10,15 +10,20 @@ import java.util.Date;
 
 public class DateConvert {
 
-    public static String getDate(Date date){
-        return new SimpleDateFormat("HH:mm:ss dd MM yyyy").format(date);
+    public static String getDateToString(Date date){
+        String result = "";
+
+        if(date != null)
+            result = new SimpleDateFormat("HH:mm:ss dd.MM.yyyy").format(date);
+
+        return result;
     }
 
     public static Date getDate(String date) throws ParseException {
 
         if(date.length() > 8) {
             SimpleDateFormat format = new SimpleDateFormat();
-            format.applyPattern("HH:mm:ss dd.mm.yyyy");
+            format.applyPattern("HH:mm:ss dd.MM.yyyy");
             return format.parse(date);
         } else {
             return null;
