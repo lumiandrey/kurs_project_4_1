6fb6db1f08792e497e3bdb085a7aa5d17cffce7c
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-12-02 12:47:27 +0300

    add class init db table Country

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/startdata/CountryStorageInit.java
@@ -0,0 +1,24 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.startdata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.entity.CountryEntity;
+
+/**
+ * Created by Andrey on 02.12.2016.
+ */
+
+public class CountryStorageInit {
+
+    private List<CountryEntity> mEntityList = new ArrayList<>();
+
+    public List<CountryEntity> getData(){
+
+        mEntityList.add(new CountryEntity(1, "Беларусь", "BY", "375"));
+        mEntityList.add(new CountryEntity(1, "Россия", "RU", "9"));
+        mEntityList.add(new CountryEntity(1, "Украина", "BY", "375"));
+
+        return mEntityList;
+    }
+}
