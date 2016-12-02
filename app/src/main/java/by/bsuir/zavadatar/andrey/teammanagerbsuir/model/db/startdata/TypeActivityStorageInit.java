5185b978311b693c6a4a67b67c1d82f18667ae5c
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-12-02 14:54:42 +0300

    add class init db table type_activity

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/model/db/startdata/TypeActivityStorageInit.java
@@ -0,0 +1,26 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.model.db.startdata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.model.entity.TypeActivityEntity;
+
+/**
+ * Created by Andrey on 02.12.2016.
+ */
+
+public class TypeActivityStorageInit {
+
+    private List<TypeActivityEntity> mEntityList = new ArrayList<>();
+
+    public List<TypeActivityEntity> getData() {
+
+        mEntityList.add(new TypeActivityEntity(1, "Developer"));
+        mEntityList.add(new TypeActivityEntity(2, "Education"));
+        mEntityList.add(new TypeActivityEntity(3, "Testing"));
+        mEntityList.add(new TypeActivityEntity(4, "Review"));
+        mEntityList.add(new TypeActivityEntity(5, "Management"));
+
+        return mEntityList;
+    }
+}
