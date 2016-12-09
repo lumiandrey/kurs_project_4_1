174e35230d9d2770eb1de12cb3184eff1c58d576
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-12-09 23:36:17 +0300

    create activity logtime

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/activity/LogTimeSingleActivity.java
@@ -0,0 +1,48 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.activity;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.fragment.LogTimeFragment;
+import by.bsuir.zavadatar.andrey.teammanagerbsuir.fragment.Operation;
+
+public class LogTimeSingleActivity extends PersonUseApplicationActivity {
+
+    private static final String TAG = LogTimeSingleActivity.class.getName();
+
+    private static final String ARG_NAME = "Name task";
+    private static final String ARG_ID_TASK = "Task id";
+
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        if(getIntent().getExtras() != null)
+            super.replaceFrame(LogTimeFragment.newInstance(
+                    getIntent().getExtras().getString(ARG_NAME),
+                    getIntent().getExtras().getInt(ARG_ID_TASK),
+                Operation.CREATE) );
+
+        Log.d(TAG, "create LogTimeSingleActivity");
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        Log.d(TAG, "get Fragment");
+        return null;
+    }
+
+    public static Intent newIntent(Context packageContext, String nameTask, Integer idTask) {
+        Intent intent = new Intent(packageContext, LogTimeSingleActivity.class);
+
+        intent.putExtra(ARG_NAME, nameTask);
+        intent.putExtra(ARG_ID_TASK, idTask);
+
+        return intent;
+    }
+}
