74a26ba6a00af1f9507e374fba9386af31e3f49f
Andrey.Martynenko <lumiandreylive@gmail.com>
2016-12-09 23:35:14 +0300

    create type operation enum

---
+++ app/src/main/java/by/bsuir/zavadatar/andrey/teammanagerbsuir/fragment/Operation.java
@@ -0,0 +1,12 @@
+package by.bsuir.zavadatar.andrey.teammanagerbsuir.fragment;
+
+
+import java.io.Serializable;
+
+public enum Operation implements Serializable {
+
+    CREATE,
+    SHOW_OR_UPDATE,
+    SHOW
+
+}
